8be31b2f9dac2fce5df50bfda8e03d7c90e2b4ef
Manabu Sakai <rea.527@gmail.com>
2018-03-15 08:44:04 +0900

    Add logger code

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'fluent-logger'
+
+logger = Fluent::Logger::FluentLogger.new(nil, :host => 'localhost', :port => 24224)
 
 get '/' do
+  logger.post('app.access', {'path' => '/'})
   'Hello world! via ' + ENV['HOSTNAME']
 end
